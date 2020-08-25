c97a696fb4bde960608b4879b3163067d8c4169a
DottaMP <dotta.may@gmail.com>
2020-08-24 21:09:42 -0300

    Add na Master

---
+++ SobreMimComHTML-CSS/assets/js/script.js
@@ -0,0 +1,83 @@
+/* 
+var é um escopo global
+let escopo local (a mais usada)
+const escopo globol, porém é constante, ou seja, não muda seu valor*/
+/* JS é uma linguagem case sensitive = reconhece letras maiusculas e minusculas
+por nome = window.document.getElementById("nome")
+pode também ser acesso por Tag> getElementByTagName()
+por id: getElementById()
+por nome: getElementsByName()
+por Classe: getElementsByClassName()
+por Seletor: querySelector("#") *será a mais utilizada */
+
+let nome = window.document.getElementById("nome")
+let email = document.querySelector("#email")
+let assunto = document.querySelector("#assunto")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector("#mapa")
+
+nome.style.width = "100%"
+email.style.width = "100%"
+assunto.style.width = "100%"
+
+function validaNome() {
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length <3) {
+        txtNome.innerHTML='Nome Inválido'
+        txtNome.style.color= 'red'
+    } else {
+        txtNome.innerHTML='Nome Válido'
+        txtNome.style.color= 'green'
+        nomeOk = true
+    }
+
+}
+
+function validaEmail() {
+    let txtEmail= document.querySelector('#txtEmail')
+
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'E-mail Inválido'
+        txtEmail.style.color= 'red'
+    } else {
+        txtEmail.innerHTML = 'E-mail Válido'
+        txtEmail.style.color= 'green'
+        emailOk = true
+    }
+}
+
+function validaAssunto(){
+    let txtAssunto = document.querySelector("#txtAssunto")
+
+    if (assunto.value.length >= 100) {
+        txtAssunto.innerHTML = "Texto muito grande, digite no máximo 100 caracteres"
+        txtAssunto.style.color= 'red'
+        //txtAssunto.style.backgroundColor = 'gray' *para deixar o fundo colorido
+        txtAssunto.style.display = "block"  
+    } else {
+        txtAssunto.innerHTML = 'Texto Válido'
+        txtAssunto.style.color= 'green'
+        txtAssunto.style.display = "none"   
+        assuntoOk = true
+    }
+}
+
+function enviar() {
+    if(nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert ("Formulário enviado com sucesso!")
+    } else {
+        alert ("Preencha o formulário corretamente!")
+    }
+}
+
+function mapaZoom(){
+    mapa.style.width = '900px'
+    mapa.style.height = '600px'
+}
+
+function mapaNormal(){
+    mapa.style.width = "500px"
+    mapa.style.height = "350px"
+}
